a3ec750e9e380fef3aa2598e4bb336ba59e14b02
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2016-02-22 08:18:08 +0100

    Fix donetext

--- src/clockpicker.js
+++ src/clockpicker.js
@@ -367,7 +367,7 @@
 		fromnow: 0,          // set default time to * milliseconds from now (using with default = 'now')
 		placement: 'bottom', // clock popover placement
 		align: 'left',       // popover arrow align
-		donetext: '完�??',    // done button text
+		donetext: '完成',    // done button text
 		autoclose: false,    // auto close when minute is selected
 		twelvehour: false, // change to 12 hour AM/PM clock from 24 hour
 		vibrate: true        // vibrate the device when dragging clock hand
